3dd1917f0e139b0b0df62d09f970137bf4e12d76
onileowotimmy77-art <onileowotimmy77@gmail.com>
2026-07-21 19:49:24 +0100

    Auto-commit: src/features/product/gallery/GalleryImage.tsx at 7/21/2026, 7:49:22 PM

--- src/features/product/gallery/GalleryImage.tsx
+++ src/features/product/gallery/GalleryImage.tsx
@@ -33,9 +33,13 @@ export default function GalleryImage({
         src={image}
         alt={alt}
         fill
+        quality={90}
+        sizes="(max-width: 1024px) 100vw, 50vw"
         className="
           object-cover
-          
+          will-change-transform
+          transition-transform
+          duration-700
         "
       />
 
@@ -43,7 +47,11 @@ export default function GalleryImage({
         className="
           absolute
           inset-0
-          bg-black/10
+          pointer-events-none
+          bg-gradient-to-t
+          from-black/12
+          via-transparent
+          to-transparent
         "
       />
     </div>
