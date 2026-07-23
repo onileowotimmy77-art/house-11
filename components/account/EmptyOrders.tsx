326b507b96324fd3fa98768fb44840df2282d609
onileowotimmy77-art <onileowotimmy77@gmail.com>
2026-07-23 10:26:16 +0100

    Add EmptyOrders component to display a message when no orders are present

---
+++ components/account/EmptyOrders.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import CommerceButton from "@/components/commerce/CommerceButton";
+
+import {
+  Display,
+  Body,
+} from "@/components/ui/typography";
+
+export default function EmptyOrders() {
+  return (
+    <section
+      className="
+        flex
+        flex-col
+        items-center
+        justify-center
+
+        py-32
+
+        text-center
+      "
+    >
+      <Display>
+        Nothing Yet
+      </Display>
+
+      <Body
+        className="
+          mt-8
+          max-w-xl
+          text-white/55
+        "
+      >
+        Every House begins somewhere.
+        Your future orders will appear here once your first
+        Residence has been reserved.
+      </Body>
+
+      <CommerceButton
+        href="/collections"
+        className="mt-16"
+      >
+        Explore Collections
+      </CommerceButton>
+
+    </section>
+  );
+}
